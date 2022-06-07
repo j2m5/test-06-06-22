0a1c335708f065576ef0acc67ec94daa29c1ff91
Дмитрий <roffits@gmail.com>
2022-06-07 16:10:36 +0300

    initial

---
+++ database/migrations/2022_06_06_074535_create_vehicles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('driver_id')->nullable();
+            $table->string('model');
+            $table->tinyInteger('category');
+            $table->bigInteger('busy_by')->nullable();
+            $table->timestamp('busy_at')->nullable();
+            $table->timestamp('busy_until')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
